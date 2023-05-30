025d973ff7446194a53a30da8673cf7536029495
faan-isme <faanisme@gmail.com>
2023-05-30 22:11:17 +0700

    update view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,6 @@
                         <div class="r w-14 h-14 overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
                            <img class="w-14 h-14 -mb-1" src="{{asset('storage/user-solid.svg')}}" alt="logo"> 
                         </div>
-                        
                     </div>
                     <br>
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -30,7 +29,7 @@
                             <div>
                             <span class="font-medium">Danger alert!</span> 
                             <ul>
-                                @foreach ($errors-all() as $error)
+                                @foreach ($errors->all() as $error)
                                     <li>{{$error}}</li>
                                 @endforeach
                             </ul>
@@ -42,7 +41,7 @@
                         @csrf
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@example.com" required="">
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@example.com" required="" value="{{Session::get('email')}}">
                         </div>
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -51,7 +50,7 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-start">
                                 <div class="flex items-center h-5">
-                                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" >
                                 </div>
                                 <div class="ml-3 text-sm">
                                   <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
@@ -61,7 +60,7 @@
                         </div>
                         <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Dont have an account yet? <a href="/register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                            Dont have an account yet? <a href="/register" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
                         </p>
                     </form>
                 </div>
