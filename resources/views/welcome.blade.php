7301c2020d4a7c8e207237d03c66cc7bce007af7
faan-isme <faanisme@gmail.com>
2023-05-30 21:03:32 +0700

    menambah view error massage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,16 +10,36 @@
 <body>
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                <img class="w-8 h-8 mr-2" src="{{asset('storage/user-solid.svg')}}" alt="logo">
-                Login   
-            </a>
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <div class="flex  justify-center">
+                        <div class="r w-14 h-14 overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
+                           <img class="w-14 h-14 -mb-1" src="{{asset('storage/user-solid.svg')}}" alt="logo"> 
+                        </div>
+                        
+                    </div>
+                    <br>
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Sign in to your account
-                    </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    </h1><br>
+                    {{-- error massages --}}
+                    @if ($errors->any())
+                       <div class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                            <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Info</span>
+                            <div>
+                            <span class="font-medium">Danger alert!</span> 
+                            <ul>
+                                @foreach ($errors-all() as $error)
+                                    <li>{{$error}}</li>
+                                @endforeach
+                            </ul>
+                            </div>
+                        </div> 
+                    @endif
+                    
+                    <form class="space-y-4 md:space-y-6" action="/login" method="POST">
+                        @csrf
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                             <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@example.com" required="">
