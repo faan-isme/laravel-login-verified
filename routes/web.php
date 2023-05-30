d4b75f929f590a6a89233b4e18601897d82ea9a0
faan-isme <faanisme@gmail.com>
2023-05-31 00:51:02 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', [SessionController::class, 'index'])->name('index');
 Route::get('/register',[SessionController::class,'register'])->name('register');
 Route::get('/home',[HalamanController::class,'home'])->name('home')->middleware('auth');
 Route::post('/login', [SessionController::class, 'login'])->name('login');
+Route::post('/create', [SessionController::class, 'create'])->name('create');
 Route::get('/login', [SessionController::class, 'index']);
+Route::get('/create', [SessionController::class, 'register']);
