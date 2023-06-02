df35f7ec5199e8245755b7c69a38ba23ff605fc3
faan-isme <faanisme@gmail.com>
2023-06-02 16:05:25 +0700

    menambah fungsi resend verifikasi

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-
+use Illuminate\Http\Request;
 
 class VerificationController extends Controller
 {
@@ -15,5 +15,11 @@ class VerificationController extends Controller
     {
         $request->fulfill();
         return redirect()->route('home');
+    }
+
+    function resendVerif(Request $request)
+    {
+        $request->user()->sendEmailVerificationNotification();
+        return redirect()->route('verification.notice')->with('massage', 'Email Verifikasi sudah terkirim!');
     }
 }
